72c6256059226698201b5d04b6319a087f5246f1
yoraish <yorai.shaoul@gmail.com>
2019-06-09 20:25:29 -0400

    Changing structure to Class. Major overhaul.

--- src/cpp/bot.cpp
+++ src/cpp/bot.cpp
@@ -16,15 +16,133 @@
 using namespace std;
 using namespace cv;
 
-int camera_feeder()
+/////////////////////////////////
+/*
+* TODO
+* create debug console
+*/
+
+class RaspiBot
+{
+private:
+    /* data */
+public:
+    // the public variables and methods
+    // only constructors and default values
+    RaspiBot(bool imgproc);
+    bool use_preview = true;
+    int fd;
+    ~RaspiBot();
+    void send_command(string cmd);
+    int handle_command();
+    int camera_feeder();
+};
+
+RaspiBot::RaspiBot(bool preview = true)
+{
+    // initialization function
+    // initialize public variables
+    this->use_preview = preview;
+
+    // initializes the Arduino
+    // starts connection, and sends a short movement
+    // this is mostly a sanity check that the arduino is connected
+    //start serial
+    fd = serialOpen("/dev/ttyACM0", 9600);
+    if (fd < 0)
+    {
+        fprintf(stderr, "* Unable to open serial device: %s\n", strerror(errno));
+    }
+    else
+    {
+        usleep(2000000); // sleep for 5 ms
+
+        cout << "ok we have arduino" << endl;
+        send_command("rit");
+        usleep(50000); // sleep for 5 ms
+        send_command("lft");
+        usleep(50000); // sleep for 5 ms
+        send_command("stp");
+
+        cout << "* Arduino connection initialized" << endl;
+    }
+
+    // spin the keyboard command thread
+    cout << "* Spinning the command thread. Detaching from main thread." << endl;
+    thread command_thread(&RaspiBot::handle_command, this);
+    command_thread.detach();
+
+    // spin the camera preview thread (on screen live window), if asked for
+    if (use_preview == true)
+    {
+        cout << "* Spinning the preview thread, in main thread" << endl;
+        thread preview_thread(&RaspiBot::camera_feeder, this);
+        preview_thread.join();
+    }
+
+    // keep Spinning
+    while(true){}
+
+}
+
+RaspiBot::~RaspiBot()
+{
+    cout << "* Closing Raspi Bot" << endl;
+}
+
+void RaspiBot::send_command(string cmd)
+{
+    // function that sends commands to the Arduino
+    // cmd in "fwd", "bwd", "rit", "lft", "stp"
+    // fd is the channel we are communicating on
+    // int fd = serialOpen("/dev/ttyACM0", 9600);
+    string str_cmd;
+
+    if (cmd == "fwd")
+    {
+        str_cmd = "w\n";
+    }
+    else if (cmd == "bwd")
+    {
+        str_cmd = "s\n";
+    }
+    else if (cmd == "rit")
+    {
+        str_cmd = "d\n";
+    }
+    else if (cmd == "lft")
+    {
+        str_cmd = "a\n";
+    }
+    else
+    {
+        str_cmd = "e\n"; //stop
+    }
+
+    //send it
+    const char *cmd_ptr = str_cmd.c_str();
+    serialPuts(fd, cmd_ptr);
+
+    // get the response
+    usleep(50000); // sleep for 5 ms
+
+    // get the response
+    string back_data = "";
+    while (serialDataAvail(fd) > 0)
+    {
+        back_data += serialGetchar(fd);
+    }
+}
+
+int RaspiBot::camera_feeder()
 {
     // function that runs and updates a window on the desktop
     // set up camera
     // initializing camera feed
     VideoCapture cap(0);
-    cap.set(CV_CAP_PROP_BUFFERSIZE, 1); // internal buffer will now store only 3 frames
-    cap.set(CV_CAP_PROP_FRAME_WIDTH,320); // experimenting with this and the next lines
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT,240);
+    cap.set(CV_CAP_PROP_BUFFERSIZE, 1);    // internal buffer will now store only 3 frames
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 320); // experimenting with this and the next lines
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
 
     if (!cap.isOpened())
     {
@@ -65,17 +183,12 @@ int camera_feeder()
     cout << "bye!" << endl;
 }
 
-int handle_command()
+int RaspiBot::handle_command()
 {
     // function that handles keyboard commands
+    // sends the commands to the Arduino, which moves the robot correctly
     // initializing serial
-    int fd;
-
-    if ((fd = serialOpen("/dev/ttyACM0", 9600)) < 0)
-    {
-        fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
-        return 1;
-    }
+    int channel = serialOpen("/dev/ttyACM0", 9600);
 
     // loop, wait for key. When got key, serially send it to Arduino
     while (true)
@@ -89,143 +202,40 @@ int handle_command()
         const char *c = command.c_str();
 
         // send serially to Arduino
-        serialPuts(fd, c);
+        serialPuts(channel, c);
         cout << "wrote command=" << command << endl;
         // let the message bounce for a moment
         usleep(50000); // sleep for 5 ms
 
         // get the response
         string data = "";
-        while (serialDataAvail(fd) > 0)
+        while (serialDataAvail(channel) > 0)
         {
-            data += serialGetchar(fd);
+            data += serialGetchar(channel);
         }
-        cout << "got=[" << data << "]" << endl;
+        // cout << "got=[" << data << "]" << endl; // TODO move into debug console
     }
 }
 
-
-
 int main(int argc, char **argv)
 {
-    bool use_cam = false;
-    if (argc > 1 ){
+
+    // check if want to use cam or not
+    bool use_cam = true;
+    if (argc > 1)
+    {
         // then something was specified about showing an image
         // argv[0] = 0 means no camera
         // argv[1] = 1 or non existing means camera
-        cout << "this is " << argv[1] << endl;
-        if (*argv[1] == '0'){
+        cout << "* Use preview = " << argv[1] << endl;
+        if (*argv[1] == '0')
+        {
             use_cam = false;
         }
     }
 
-    if (use_cam == true){
-        thread camera_thread(camera_feeder);
-        camera_thread.join();
-    }
+    // create a new class instance
+    RaspiBot bot(use_cam);
 
-    thread command_thread(handle_command);
-    command_thread.join();
     return -1;
 }
-
-// // initializing camera feed
-// VideoCapture cap(0);
-// cap.set(CV_CAP_PROP_BUFFERSIZE, 1); // internal buffer will now store only 3 frames
-
-// if (!cap.isOpened())
-// {
-//     cerr << "ERROR: Unable to open the camera" << endl;
-//     return 0;
-// }
-
-// Mat frame;
-// cout << "Start grabbing, press a key on Live window to terminate" << endl;
-
-// // initializing serial
-// int fd;
-
-// if ((fd = serialOpen("/dev/ttyACM0", 9600)) < 0)
-// {
-//     fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
-//     return 1;
-// }
-
-// // Loop, getting and printing characters
-// char a = 'b';
-// while (true)
-// {
-
-//     // capture image
-//     cap >> frame;
-//     if (frame.empty())
-//     {
-//         cerr << "ERROR: Unable to grab from the camera" << endl;
-//         break;
-//     }
-
-//     // flip frame
-//     Mat hflipped;
-//     // horizonatally
-//     flip(frame, hflipped, 1);
-//     // vertically
-//     Mat final_frame;
-//     flip(hflipped, final_frame, 0);
-
-//     imshow("Live", final_frame);
-//     int key = cv::waitKey(30);
-//     key = (key == 255) ? -1 : key; //#Solve bug in 3.2.0
-//     if (key >= 0)
-//     {
-//         char command_char = char(key);
-//         string command(1, command_char);
-//         command += "\n";
-//         const char *c = command.c_str();
-
-//         serialPuts(fd, c);
-//         cout << "wrote command=" << command << endl;
-//         // let the message bounce for a moment
-//         usleep(50000); // sleep for 5 ms
-
-//         // get the response
-//         string data = "";
-//         while (serialDataAvail(fd) > 0)
-//         {
-//             data += serialGetchar(fd);
-//         }
-//         cout << "got=[" << data << "]" << endl;
-
-//         cout << "loop" << endl;
-//     }
-
-//     // // ask for command
-//     // cout << "write command >>>";
-//     // string command;
-//     // cin >> command;
-//     // command += "\n";
-//     // convert command to const char pointer
-//     // const char *c = command.c_str();
-
-//     // send serially to Arduino
-//     // serialPuts(fd, c);
-//     // cout << "wrote command=" << command << endl;
-//     // // let the message bounce for a moment
-//     // usleep(50000); // sleep for 5 ms
-
-//     // // get the response
-//     // string data = "";
-//     // while (serialDataAvail(fd) > 0)
-//     // {
-//     //     data += serialGetchar(fd);
-//     // }
-//     // cout << "got=[" << data << "]" << endl;
-
-//     // fflush(stdout);
-//     // serialFlush(fd);
-// }
-
-// cout << "Closing the camera" << endl;
-// cap.release();
-// destroyAllWindows();
-// cout << "bye!" << endl;
-// return 0;
